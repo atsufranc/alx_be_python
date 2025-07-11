1255983ca34ad3564088604f4435ea09f6aecc32
atsufranc <atsufranc200363@gmail.com>
2025-07-11 20:44:59 +0000

    adding simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculating interest (I = P * R * T)
+
+interest = principal * rate * time
+
+#print interest to the console
+print("The simple interest is: ",interest)
