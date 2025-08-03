eb1ecbe7a062337d71dbd875d69b5317fd8db932
atsufranc <atsufranc200363@gmail.com>
2025-08-03 20:49:35 +0000

    adding programingparadign

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,13 +9,13 @@ class BankAccount:
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
-            print(f"Withdrew: ${amount}")
-            return True 
-        print("Insufficient funds.") 
-        return False
+            #print(f"Withdrew: ${amount}")
+            return f"Withdrew: ${amount}" 
+        #print("Insufficient funds.") 
+        return f"Insufficient funds."
     def get_balance(self):
         return self.balance
-    def __str__(self):
-        return f"Balance={self.balance})"
+    # def __str__(self):
+    #     return f"Balance={self.balance})"
     def display_balance(self):
         return f"Current Balance: ${self.balance}"
